1204bc7501e2ecca298b0572243b5225c8e4b683
Josh Brunner <joshuaebrunner@gmail.com>
2016-11-19 23:13:06 -0800

    initial commit of filebase

---
+++ navbar.js
@@ -0,0 +1,7 @@
+document.write('<li class="divider-vertical"></li><li class=""><a href="./jewelry.html">Jewelry</a></li>');
+document.write('<li class="divider-vertical"></li><li class=""><a href="./clothing.html">Clothing</a></li>');
+document.write('<li class="divider-vertical"></li><li class=""><a href="./footwear.html">Footwear</a></li>');
+document.write('<li class="divider-vertical"></li><li class=""><a href="./coffee.html">Coffee</a></li>');
+document.write('<li class="divider-vertical"></li><li class=""><a href="./reviews.html">Reviews</a></li>');
+document.write('<li class="divider-vertical"></li><li class=""><a href="./about.html">About</a></li>');
+document.write('<li class="divider-vertical"></li><li class=""><a href="./contact.html">Contact</a></li>');
